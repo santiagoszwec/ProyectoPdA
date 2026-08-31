573271567abf709cf2c196a671d31a6a99edbf88
angelina <angelinacontini@estudiantes.utec.edu.uy>
2026-08-31 16:41:43 -0300

    Iniciar Sesion

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,36 @@
 package org.example;
 
+import org.example.DAOS.UsuarioDAO;
+import org.example.Modelos.Usuario;
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         Migracion.migrar();
+
+        Scanner sc = new Scanner(System.in);
+        Usuario usuario;
+
+        do{
+            System.out.print("Correo: ");
+            String correo = sc.nextLine();
+            System.out.print("Contrasenia: ");
+            String contrasenia = sc.nextLine();
+
+            usuario = UsuarioDAO.iniciarSesion(correo, contrasenia);
+
+            if(usuario == null){
+                System.out.print("Correo o contrasenia incorrectos. Intente de nuevo");
+            }
+
+        }while(usuario == null);
+
+        System.out.print("Bienvenido!");
+
+
     }
+
+
+
 }
