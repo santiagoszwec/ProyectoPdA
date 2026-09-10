a896fb7d978653ee1537c172b5da32f9f9649e81
Nigel5002 <134554504+Nigel5002@users.noreply.github.com>
2026-09-09 22:36:27 -0300

    cambiando nombre para que se entienda

--- src/main/java/org/example/DAOS/NotificacionDAO.java
+++ src/main/java/org/example/DAOS/NotificacionDAO.java
@@ -11,13 +11,13 @@ public class NotificacionDAO {
 
     public static boolean crear(Notificacion notificacion) {
         try (Connection conexion = ConexionDB.obtenerConexion()) {
-            return crear(conexion, notificacion);
+            return insertarNotificacion(conexion, notificacion);
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
     }
 
-    public static boolean crear(Connection conexion, Notificacion notificacion) throws SQLException {
+    public static boolean insertarNotificacion(Connection conexion, Notificacion notificacion) throws SQLException {
         String sql = "INSERT INTO notificacion (fecha, tipo, mensaje, usuario_id) VALUES (?,?,?,?)";
         try (PreparedStatement sentencia = conexion.prepareStatement(sql)) {
             sentencia.setObject(1, notificacion.getFecha());
